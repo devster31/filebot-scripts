04638690eb0b57636aa937de5537d43b96c5759d
devster31 <devster31@gmail.com>
2018-03-01 00:24:01 +0100

    use variable for FileBot executable

--- transmission-postprocess.sh
+++ transmission-postprocess.sh
@@ -12,6 +12,7 @@ ARG_LABEL="N/A"
 
 # Configuration
 CONFIG_OUTPUT="/mnt/antares/Media"
+FILEBOT="/usr/local/bin/filebot"
 
 if [[ "$TR_TORRENT_DIR" =~ ^/mnt/usbhdd.* ]]
 then
@@ -23,7 +24,6 @@ then
     exit 0
 fi
 
-
 LINKS=($(find "$ARG_PATH" -type l))
 if [[ ${#LINKS[@]} -gt 0 ]]
 then
@@ -32,10 +32,13 @@ fi
 
 transmission-remote -t ${TR_TORRENT_ID} -S
 
+export JAVA_OPTS="-Xmx256M"
 if [[ "$TR_TORRENT_DIR" =~ ^/mnt/bellatrix/downloads/(tv_shows|anime).* ]]
 then
 # exec="chmod 664 {quote file} ; docker exec rpi3_medusa curl -s -w '\n' 'http://localhost:8081/api/v1/***REMOVED***?cmd=show.refresh&tvdbid={info.id}'" \
-    sudo -H -u devster -g devster -- /usr/bin/filebot -script fn:amc --action keeplink --output "$CONFIG_OUTPUT" --conflict skip \
+# tmpfile="$(mktemp -p /dev/shm/)"
+# exec="echo {quote file},{quote f.dir.dir},{info.database},{info.id},{quote info.name} > $tmpfile"
+    sudo -H -u devster -g devster -- $FILEBOT -script fn:amc --action keeplink --output "$CONFIG_OUTPUT" --conflict skip \
         --filter '!readLines("/mnt/antares/scripts/tv_excludes.txt").contains(n)' \
         -non-strict --log-file amc.log --def artwork=y excludeList=".excludes" \
         --def ut_dir="$ARG_PATH" ut_kind="multi" ut_title="$ARG_NAME" ut_label="$ARG_LABEL" \
@@ -45,7 +48,7 @@ then
         --def seriesFormat=@/mnt/antares/scripts/seriesFormat.groovy \
         --def animeFormat=@/mnt/antares/scripts/animeFormat.groovy
 else
-    sudo -H -u devster -g devster -- /usr/bin/filebot -script fn:amc --action keeplink --output "$CONFIG_OUTPUT" --conflict skip \
+    sudo -H -u devster -g devster -- $FILEBOT -script fn:amc --action keeplink --output "$CONFIG_OUTPUT" --conflict skip \
         --filter '!readLines("/mnt/antares/scripts/movie_excludes.txt").contains(n)' \
         --log-file amc.log --def subtitles=en artwork=y excludeList=".excludes" \
         ut_dir="$ARG_PATH" ut_kind="multi" ut_title="$ARG_NAME" ut_label="$ARG_LABEL" \
