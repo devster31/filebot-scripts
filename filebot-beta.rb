a9e963f6abe1826c88afe551cd6c2e01d288f1e2
Daniele Riccucci <daniele.riccucci@gmail.com>
2019-07-05 18:33:25 +0200

    add personal mirror for filebot-beta homebrew cask formula

---
+++ filebot-beta.rb
@@ -0,0 +1,14 @@
+cask 'filebot' do
+  version '4.8.6'
+  sha256 :no_check
+  conflicts_with cask: "filebot"
+
+  url "https://get.filebot.net/filebot/BETA/FileBot_#{version}.app.tar.xz"
+  name 'FileBot Beta'
+  homepage 'https://www.filebot.net/'
+
+  app 'FileBot.app'
+  binary "#{appdir}/FileBot.app/Contents/MacOS/filebot.sh", target: 'filebot'
+
+  zap trash: '~/Library/Preferences/net.filebot.ui.plist'
+end
